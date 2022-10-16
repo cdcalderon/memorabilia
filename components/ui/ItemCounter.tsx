a3762a08272ff8dbb18663f09a04be4eb68eae13
Carlos Calderon <cdcalderon@gmail.com>
2022-10-16 15:11:15 -0500

    add layout base elements

---
+++ components/ui/ItemCounter.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const ItemCounter = () => {
+    return <div>ItemCounter</div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
+import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { ShopLayout } from '../components/layouts'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-    return <h1>Memorabilia</h1>
+    return (
+        <ShopLayout
+            title={'Memorabilia - Home'}
+            pageDescription={'The best source for memorabilia'}
+        >
+            <Typography variant="h1" component="h1">
+                Shop
+            </Typography>
+            <Typography variant="h2" sx={{ mb: 1 }}>
+                All products
+            </Typography>
+        </ShopLayout>
+    )
 }
 
 export default Home
