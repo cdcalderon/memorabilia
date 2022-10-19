0090df65d1a8f5e8a2f64e982cfb3995dd6ad339
Carlos Calderon <cdcalderon@gmail.com>
2022-10-18 22:54:54 -0500

    add to layout

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,11 @@
+import { ShopLayout } from '../components/layouts'
+
 const Custom404 = () => {
-    return <h2>Custom 404</h2>
+    return (
+        <ShopLayout title="Page not found" pageDescription="No hay nada que mostrar aquí">
+            <h2>Custom 404</h2>
+        </ShopLayout>
+    )
 }
 
 export default Custom404
