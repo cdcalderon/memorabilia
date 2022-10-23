43f1fe271a23f6ecfaa09d93baa60b9ba25ff398
Carlos Calderon <cdcalderon@gmail.com>
2022-10-22 20:04:04 -0500

    implement main logic and layput for Slug page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import { ShopLayout } from '../components/layouts'
-import styles from '../styles/Home.module.css'
 import { initialData } from '../database/products'
 import { ProductList } from '../components/products/ProductList'
 
