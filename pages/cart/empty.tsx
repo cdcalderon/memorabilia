e587dd7458442dd397bab3b49e862cb7c2c441a6
Carlos Calderon <cdcalderon@gmail.com>
2022-10-28 20:06:16 -0500

    add  RemoveShoppingCartOutlined

--- pages/cart/empty.tsx
+++ pages/cart/empty.tsx
@@ -14,6 +14,7 @@ const EmptyPage = () => {
                 height="calc(100vh - 200px)"
                 sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
             >
+                <RemoveShoppingCartOutlined sx={{ fontSize: 100 }} />
                 <Box display="flex" flexDirection="column" alignItems="center">
                     <Typography>Your shopping cart is empty</Typography>
                     <NextLink href="/" passHref>
