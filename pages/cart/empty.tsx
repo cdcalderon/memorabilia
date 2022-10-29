46a2a92a88fcb3a971b4956f63662c36578828d1
Carlos Calderon <cdcalderon@gmail.com>
2022-10-28 20:05:46 -0500

    implement empty cart page

--- pages/cart/empty.tsx
+++ pages/cart/empty.tsx
@@ -6,10 +6,24 @@ import { ShopLayout } from '../../components/layouts'
 
 const EmptyPage = () => {
     return (
-        <ShopLayout
-            title="Carrito vació"
-            pageDescription="No hay artículos en el carrito de compras"
-        ></ShopLayout>
+        <ShopLayout title="Empty" pageDescription="Empty">
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="calc(100vh - 200px)"
+                sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
+            >
+                <Box display="flex" flexDirection="column" alignItems="center">
+                    <Typography>Your shopping cart is empty</Typography>
+                    <NextLink href="/" passHref>
+                        <Link typography="h4" color="secondary">
+                            Back
+                        </Link>
+                    </NextLink>
+                </Box>
+            </Box>
+        </ShopLayout>
     )
 }
 
