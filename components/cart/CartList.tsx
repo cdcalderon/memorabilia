fed0902d457c999b4e1a25be445bcef60dd18561
Carlos Calderon <cdcalderon@gmail.com>
2022-11-09 23:01:42 -0600

    implement cartlist component

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -14,7 +14,47 @@ interface Props {
 export const CartList: FC<Props> = ({ editable = false }) => {
     return (
         <>
-            <div>CartList</div>
+            {productsInCart.map((product) => (
+                <Grid container spacing={2} key={product.slug} sx={{ mb: 1 }}>
+                    <Grid item xs={3}>
+                        {/* TODO: navigate to product page */}
+                        <NextLink href="/product/slug" passHref>
+                            <Link>
+                                <CardActionArea>
+                                    <CardMedia
+                                        image={`/products/${product.images[0]}`}
+                                        component="img"
+                                        sx={{ borderRadius: '5px' }}
+                                    />
+                                </CardActionArea>
+                            </Link>
+                        </NextLink>
+                    </Grid>
+                    <Grid item xs={7}>
+                        <Box display="flex" flexDirection="column">
+                            <Typography variant="body1">{product.title}</Typography>
+                            <Typography variant="body1">
+                                Size: <strong>M</strong>
+                            </Typography>
+
+                            {editable ? (
+                                <ItemCounter />
+                            ) : (
+                                <Typography variant="h5">3 items</Typography>
+                            )}
+                        </Box>
+                    </Grid>
+                    <Grid item xs={2} display="flex" alignItems="center" flexDirection="column">
+                        <Typography variant="subtitle1">{`$${product.price}`}</Typography>
+
+                        {editable && (
+                            <Button variant="text" color="secondary">
+                                Remover
+                            </Button>
+                        )}
+                    </Grid>
+                </Grid>
+            ))}
         </>
     )
 }
